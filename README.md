1d01e3fd879a341ac32a516c2d0edf6687d25b85
daichimonma <dem4swy@virginia.edu>
2021-11-07 21:40:37 -0500

    now saves DMM readings to 2 separate csv files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # fluke-884x-measurement
 Use Fluke 8845A DMM over telnet to measure battery cells
+
+Running it: 
+    Do "Run Python File in Terminal", just to get the line below
+        & "C:/Program Files/Python/Python310/python.exe" "c:/Users/daich/Desktop/Solar Car/GitHub stuff/Fluke/fluke-884x-measurement/main.py" 
+    Path will depend on where you have it saved. 
+    Add 2 arguments: integer for the cell number, and either "O" for open or "L" for loaded.  
+
+Setting up DMMs
+On multimeter LAN, 8845 command
+E825383
+DHCP=Off
+IP=169.254.001.002
+NETMASK=255.255.254.0
+
+E825394
+DHCP=Off
+IP=169.254.001.003
+NETMASK=255.255.254.0
+
+Host computer
+Link-local static address: 169.254.1.1/23
+
+Telnet command
+telnet 169.254.1.x 3490
+
+*IDN? // print identifier
+SYST:REM // puts meter in remote mode
+MEAS:VOLT:DC? 10 // measure DC Voltage range 10V
+ 
--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import sys
 from telnetlib import Telnet
 from multiprocessing import Process
 import time
+import csv
+import pandas
+from pandas.core import frame
+
 
 DMM = ["169.254.1.2", "169.254.1.3"]
+
+DMM0data=''
+DMM1data=''
 
 
 def connect_telnet(host, port):
@@ -15,23 +22,64 @@ def connect_telnet(host, port):
         #time.sleep(0.1)
         #print(tn.read_eager().decode('ascii'))
         time.sleep(0.1)
-        tn.write(b"MEAS:VOLT:DC? 10\r\n")
-        time.sleep(0.25)
-        print(tn.read_eager().decode('ascii'))
+        if host==DMM[0]:
+            tn.write(b"MEAS:VOLT:DC? 10\r\n")
+        elif host==DMM[1]:
+            tn.write(b"MEAS:CURR:DC? 10\r\n")
+        else:
+            print("Error. Retry")
+        time.sleep(0.3) # at least 0.25
+        # print(tn.read_eager().decode('ascii')) # this one used originally
         #print(tn.read_all().decode('ascii'))
+        # save printed data to variable
+        if host==DMM[0]:
+            global DMM0data 
+            DMM0data= tn.read_eager().decode('ascii').replace('\r\n', '')
+        elif host==DMM[1]:
+            global DMM1data
+            DMM1data = tn.read_eager().decode('ascii').replace('\r\n', '')
+        else:
+            print('Invalid host argument')
 
 
+        
+
+
+
+# change argument to cell number "O" "L"
 def main():
     args = sys.argv[1:]
     if len(args) < 1:
         print("No arguments provided.")
         exit(1)
-    print("connecting DMM", args[0])
-    dmm_index = int(args[0])
-    if dmm_index > len(DMM) or dmm_index < 0:
-        print("Invalid DMM index.")
+    print("connecting for Cell # ", args[0])
+    cell_num = int(args[0])
+    if cell_num < 0:
+        print("Invalid Cell number.")
         exit(1)
-    connect_telnet(DMM[dmm_index], 3490)
+    connect_telnet(DMM[0], 3490)
+    connect_telnet(DMM[1], 3490)
+    
+    if args[1] == 'O':
+        frame = {
+                'Cell #' : [int(args[0])],
+                'Voc'   : [DMM0data],
+            }
+        df = pandas.DataFrame(frame) 
+        df.to_csv('Voc.csv', mode='a', index=False, header=False)
+    elif args[1] == 'L':
+        frame = {
+                'Cell #' : [int(args[0])],
+                'V_load'   : [DMM0data],
+                'I_load'   : [DMM1data]
+            }
+        df = pandas.DataFrame(frame) 
+        df.to_csv('loaded.csv', mode='a', index=False, header=False)
+
+    # row_data = [args[0], DMM0data, DMM1data]
+    # with open('internalResistance.csv','w',  newline='') as csvfile:
+    #     my_writer = csv.writer(csvfile, delimiter = ' ')
+    #     my_writer.writerow(row_data)
 
 
 if __name__ == "__main__":
